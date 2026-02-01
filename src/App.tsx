ec42e3780c639c811daa21acb621312fe8a86027
Nasty-Litvin <nastylitvin78@gmail.com>
2026-02-01 23:56:32 +0300

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import Button from './components/Button/Button';
-import Menu from './components/Menu/Menu';
-import Title from './components/Title/Title';
-import Logo from './components/Logo/Logo';
-import menuData from "./data/menuHeader.json";
-import Icon from './components/Icon/Icon';
-import vkUrl from './assets/Icons/vk.svg';
-import logo from './assets/logo.svg'
+
+import Header from './components/Header/Header';
+
 
 import './App.scss';
 
 function App() {
-  const text = "Сохранить"
-  const menu = menuData?.data;
+
   return (
     <>
       <div>Hello</div>
-      <Button text={text} />
-      <Title title="заголовок" style={{fontSize: '54px'}} level={1} />
-      <Title title="заголовок" style={{fontSize: '40px'}} level={2} />
-      <Menu menu={menu} />
-      <Logo urlLogo={logo} style={{maxWidth: '155px'}} />
-      <Logo urlLogo={logo} style={{maxWidth: '117px'}} />
-      <Icon urlIcon={vkUrl} style={{width: '17px'}}/>
+      <Header />
     </>
   )
 }
