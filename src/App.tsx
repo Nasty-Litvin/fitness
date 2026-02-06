2d12f2df9401679b6a09eb6eba829cad2280f7e7
litvin-av <litvin-anastasiya91@mail.ru>
2026-02-06 17:37:26 +0300

    Component TextCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import Header from './components/Header/Header';
-
+import TextCard from './components/TextCard/TextCard';
 
 import './App.scss';
 
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <Header />
+      <TextCard title="123" className="hero"/>
+       <TextCard title="123" className="sale"/>
     </>
   )
 }
